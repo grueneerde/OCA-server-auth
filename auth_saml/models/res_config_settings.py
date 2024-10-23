1bca95e486501e8d5b0b1e694aea8fcb951a743b
Jan <jan.dziurzynski@braintec.com>
2024-10-23 10:42:21 +0200

    [t151701] global default SAML provider

--- auth_saml/models/res_config_settings.py
+++ auth_saml/models/res_config_settings.py
@@ -16,6 +16,5 @@ class ResConfigSettings(models.TransientModel):
     saml_provider_id = fields.Many2one(
         "auth.saml.provider",
         string="Default SAML Provider",
-        related="company_id.saml_provider_id",
-        readonly=False,
+        config_parameter='auth_saml.default_saml_provider_id',
     )
